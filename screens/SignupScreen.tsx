61462a255cb8ca1009b6fbe26d80e218c77144a9
FizaTariq147 <fizatariq147@gmail.com>
2026-05-19 16:10:26 +0500

    fix: increase signup timeout to 120s

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -129,7 +129,7 @@ export default function SignupScreen() {
 
     // Set up 60 second timeout
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 60000);
+   const timeoutId = setTimeout(() => controller.abort(), 120000);
 
     try {
       console.log("🔍 Signup attempt for:", email);
